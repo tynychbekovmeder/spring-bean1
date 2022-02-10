1bc342ddfad1a4343c0f56516ee0dff2004807b1
User <sdfsdfsdfsd>
2022-02-10 16:55:30 +0600

    update

--- src/main/java/peaksoft/App.java
+++ src/main/java/peaksoft/App.java
@@ -7,8 +7,20 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean = (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld helloWorld = applicationContext.getBean("helloworld",HelloWorld.class);
+        System.out.println(helloWorld.getMessage());
+
+        System.out.println(bean==helloWorld);
+
+        Cat cat = applicationContext.getBean("cat",Cat.class);
+        System.out.println(cat);
+
+        Cat cat1 = applicationContext.getBean("cat",Cat.class);
+        System.out.println(cat1);
+
+        System.out.println(cat==cat1);
     }
 }
--- src/main/java/peaksoft/AppConfig.java
+++ src/main/java/peaksoft/AppConfig.java
@@ -2,6 +2,7 @@ package peaksoft;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class AppConfig {
@@ -10,5 +11,10 @@ public class AppConfig {
         HelloWorld helloWorld = new HelloWorld();
         helloWorld.setMessage("Hello World!");
         return helloWorld;
+    }
+    @Bean(name = "cat")
+    @Scope("prototype")
+    public Cat getCat(){
+        return new Cat("John",2,"brown");
     }
 }
